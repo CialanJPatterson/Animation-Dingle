418ca3410ec18d19840223fb7fab639fe97335a3
CialanJPatterson <c00297034@setu.ie>
2025-10-20 14:44:43 +0100

    Revert "to catch sfx"
    This reverts commit 1941893b1e2ade9edaf481e618359f6c6f170c4c.

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edf99aa9c167ecf4dc4121dd5c2c8877998822145d66df77b381704a077cc497
-size 48028
+oid sha256:b3de21b21284db5cd7804f1fc6795e03633cb25c38fbaa57b66053a12d596a61
+size 46950
