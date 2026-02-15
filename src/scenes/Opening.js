201360eb3dca7598ddf4005fc231e56b9ec4d0dd
CialanJPatterson <c00297034@setu.ie>
2026-02-15 15:02:30 +0000

    fixed credits bug

--- src/scenes/Opening.js
+++ src/scenes/Opening.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91ac951818adab3c6fbf35c713cb2cd1f6ab474ce502cfb4cb15bc2a0fb5a2b4
-size 9936
+oid sha256:f6ed98ac189526222ceabcc2a8a15285fdc41c0273d6211ee89b3102a614aad0
+size 10888
